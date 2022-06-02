6585fcaa51e53f59e3a0174e623a89cd735ba21e
Daniel-Dodd <daniel_dodd@icloud.com>
2022-06-02 15:24:00 +0100

    Update __init__.py

--- spans/__init__.py
+++ spans/__init__.py
@@ -2,7 +2,7 @@ from .types import SpanData
 from .utils import Scaler, RegressorMetrics, ClassifierMetrics
 from .kernels import DriftKernel
 from .simulate import simulate_bernoulli, simulate_gaussian
-from .optimal_design import pred_entropy, pred_information, box_design, box_reveal, inpsection_region_reveal, inpsection_region_design
+from .optimal_design import pred_entropy, pred_information, box_design, box_reveal, inpsection_region_reveal, inspection_region_design
 from .models import bernoulli_svgp, gaussian_svgp
 from .plots import plot_rocpr, plot_mse, plot_pred, add_drift
 from .old_kernels import OldDriftKernel, OldDriftMatern12, OldDriftMatern32, OldDriftMatern52

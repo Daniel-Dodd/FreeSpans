ca7016ff8452093b7c5ed81647b608be27b8d76c
Daniel-Dodd <daniel_dodd@icloud.com>
2022-06-06 17:15:57 +0100

    Update tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,25 @@
 import jax.numpy as jnp
-from typing import Optional
 
-from gpjax.types import Array
-from gpjax.kernels import Kernel
-from gpjax.config import Softplus, add_parameter
 
-from chex import dataclass
-import distrax as dx
+import gpjax as gpx
 
 import pytest
+import spans
+from jax import vmap
 
+@pytest.mark.parametrize("num_inducing", [1, 2, 10])
+def test_kmeans(num_inducing):
+    # Create pipe locations and time indicies:
+    L = jnp.linspace(-1, 1, 10)
+    T = jnp.linspace(-5, 5, 20)
+    
+    # Create covariates:
+    X = vmap(lambda t: vmap(lambda l: jnp.array([t,l]))(L))(T).reshape(-1, 2)
 
-def test_kernel_init():
-    assert 1 == 1
+    # Create labels
+    y = (jnp.sin(jnp.linspace(-jnp.pi/2, jnp.pi/2, 10*20)) < 0).astype(jnp.float32).reshape(-1, 1)
+    D = gpx.Dataset(X=X, y=y)
+
+    z = spans.models.kmeans_init_inducing(D, num_inducing)
+
+    assert z.shape == (num_inducing, 2)

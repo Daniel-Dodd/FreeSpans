ebd0e8967c811719c49c7644f3fed1aa80ed1e88
Daniel-Dodd <daniel_dodd@icloud.com>
2022-06-06 16:47:26 +0100

    Update test_kernels.py

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -1,15 +1,25 @@
 import jax.numpy as jnp
-from typing import Optional
+import gpjax as gpx
 
-from gpjax.types import Array
 from gpjax.kernels import Kernel
-from gpjax.config import Softplus, add_parameter
+import spans
 
-from chex import dataclass
-import distrax as dx
 
 import pytest
 
 
-def test_kernel_init():
-    assert 1 == 1
+@pytest.mark.parametrize("base", [gpx.RBF(active_dims=[0, 1]), gpx.Matern12(active_dims=[0, 1]), gpx.Matern32(active_dims=[0, 1]), gpx.Matern52(active_dims=[0, 1])])
+def test_kernel_call(base):
+
+    base_params = base.params
+    drift_params = {"theta": jnp.array([jnp.pi/2.]), "scale": jnp.array([1.])}
+  
+    kernel = spans.kernels.DriftKernel(base_kernel = base)
+
+    assert isinstance(kernel, Kernel)
+    
+
+    params = gpx.config.get_defaults()
+    assert "theta" in params["transformations"].keys()
+    assert "scale" in params["transformations"].keys()
+    assert kernel.params == {**drift_params, **base_params}
